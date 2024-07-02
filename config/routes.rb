89fc68e975ca73f8d8229e14fb54a5e62b088ecc
EC2 Default User <ec2-user@ip-172-31-39-110.ec2.internal>
2024-07-02 10:51:03 +0000

    [bookers2-6-1]

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-    resource :favorite, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
  
